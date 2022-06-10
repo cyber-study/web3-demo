d3f6b8b985b7fcebe90ba21bbcbd0ddb062696a1
yushicheng <1542874601@qq.com>
2022-06-10 21:46:39 +0800

    temp:临时节点标记 星期五 晚上 9:46:39 六月 10日 2022 修改了 src/actions/transaction_command.js

--- src/actions/transaction_command.js
+++ src/actions/transaction_command.js
@@ -9,7 +9,7 @@ export async function transaction_command() {
   try {
     const accounts = await web3.eth.getAccounts();
     const lock_status = await web3.eth.personal.unlockAccount(accounts[0], "glyz205070410");
-    console.log(accounts[0], "lock_status", lock_status);
+    console.log(accounts[0], "unlock_status", lock_status);
     const transaction_hash = await web3.eth.sendTransaction({
       from: accounts[0],
       to: accounts[1],
